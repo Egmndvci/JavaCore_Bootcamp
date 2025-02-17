d04b1a74f03e9f9fa2112c269744970b8372fe14
Egemen Deveci <egemendeveci170@gmail.com>
2025-02-17 17:47:23 +0300

    Examples

--- src/main/java/com/egemendeveci/_notes_/_00_2Example.java
+++ src/main/java/com/egemendeveci/_notes_/_00_2Example.java
@@ -8,6 +8,11 @@ public class _00_2Example {
         //F = C * 1.8 + 32
         Scanner scanner = new Scanner(System.in);
         System.out.println(("Lütfen Stantigrat(celsius) değerini giriniz:"));
+        int C= scanner.nextInt();
+        int F = C * 9/5 + 32 ;
+        System.out.println("Fahrenheit cinsinden değeri:"+ F );
+        scanner.close();
+
 
     }
 }
