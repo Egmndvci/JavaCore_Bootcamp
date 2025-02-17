bd35adbc43274e09049fc3e6a8e593850e66c63f
Egemen Deveci <egemendeveci170@gmail.com>
2025-02-17 11:29:49 +0300

    all updated

--- src/main/java/com/egemendeveci/_1_week/_04_JavaWrapperTypes.java
+++ src/main/java/com/egemendeveci/_1_week/_04_JavaWrapperTypes.java
@@ -2,6 +2,35 @@ package com.egemendeveci._1_week;
 
 public class _04_JavaWrapperTypes {
     public static void main(String[] args) {
+        //primitivetype null veremezsiniz. Daha çok primitive kullanıyoruz çünkü wrapper type'lar daha fazla yer kaplar.
+        short primitiveShort = 10;
+        int primitiveInt = 10;
+        long primitiveLong = 10;
+        float primitiveFloat = 10.0f;
+        double primitiveDouble = 10.0;
+        boolean primitiveBoolean = true;
+        char primitiveChar = 'a';
+        // WRAPPER TYPES NEW YOKSA
+        //Wrapper Types null veremezsiniz
+        Byte wrapperTypeByte= 127;
+        wrapperTypeByte=null;
+        System.out.println(wrapperTypeByte);
+        short wrapperShort = 10;
+        Integer wrapperInt = 10;
+        Long wrapperLong = 10L;
+        Float wrapperFloat = 10.0f;
+        Double wrapperDouble = 10.0;
+        Boolean wrapperBoolean = true;
+        Character wrapperChar = 'a';
 
+        // WRAPPER TYPES NEW VARSA (eskiden new ile oluşturuluyordu şimdi new kullanılmıyor)
+        Byte wrapperTypeByte = new Byte((byte) 127);
+        Short wrapperShort = new Short((short) 10);
+        Integer wrapperInt = new Integer(10);
+        Long wrapperLong = new Long(10);
+        Float wrapperFloat = new Float(10.0f);
+        Double wrapperDouble = new Double(10.0);
+        Boolean wrapperBoolean = new Boolean(true);
+        Character wrapperChar = new Character('a');
     }
 }
