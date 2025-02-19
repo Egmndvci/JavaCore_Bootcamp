58c495f043c596d4160a839912e25b882f9e8228
Egemen Deveci <egemendeveci170@gmail.com>
2025-02-19 14:39:21 +0300

    Examples

---
+++ src/main/java/com/egemendeveci/_2_week/_11_1_return_continue_break.java
@@ -0,0 +1,7 @@
+package com.egemendeveci._2_week;
+
+public class _11_1_return_continue_break {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/egemendeveci/_2_week/week2_examples_3.java
+++ src/main/java/com/egemendeveci/_2_week/week2_examples_3.java
@@ -5,22 +5,30 @@ import java.util.Scanner;
 public class week2_examples_3 {
     public static void main(String[] args) {
         //kullanıcının verdiği pozitif bir sayıya kadar toplama yapan algoritma
-        //şart 1: kullanıcı 100 den buyuk sayı girerse 100 e kadar toplasın
-        //sart 2: kullanıcının verdigi sayılar icinde 47 varsa bunu toplamaya dahil etmesin.
+        //şart 1: kullanıcı 100 den buyuk sayı girerse 100 e kadar toplasın (break)
+        //sart 2: kullanıcının verdigi sayılar icinde 47 varsa bunu toplamaya dahil etmesin.(continue)
+        //cikan sonuc tek mi cift mi?
         Scanner scanner = new Scanner(System.in);
         System.out.println("Lütfen bir sayı giriniz:");
-        int number = scanner.nextInt();
-        int toplam = 0;
+        int number = Math.abs(scanner.nextInt());//sayi negatif verirse fln mutlak degerle pozitife donusturmek icinn
+        int sum = 0;
         for (int i = 1; i <= number; i++) {
+            //eger 47 varsa toplama
             if (i == 47) {
                 continue; // 47'yi atla
             }
             if (i > 100) {
-                break; // 100'den büyük sayıları toplama
+                break; // 100'den büyük sayılarda toplamauyı durdur.
             }
-            toplam += i;
+            sum+= i;
+            //sum = sum+i de yazabilirsin
         }
-        System.out.println("Verdiğin sayıya kadar toplam: " + toplam);
+        System.out.println("Verdiğin sayıya kadar toplam: " + sum);
+        if (number%2==0){
+            System.out.println("çıkan sonuc aynı zamanda cifttir.");
+        }else{
+            System.out.println("çıkan sonuç aynı zamanda tektir.");
+        }
         scanner.close();
     }
 }
