39527c47c9a2054933fd0bc9db89140cda0818cb
Egemen Deveci <egemendeveci170@gmail.com>
2025-02-22 18:34:09 +0300

    Methods Return

--- src/main/java/com/egemendeveci/_2_week/_13_2_MethodIsReturn.java
+++ src/main/java/com/egemendeveci/_2_week/_13_2_MethodIsReturn.java
@@ -13,8 +13,10 @@ public class _13_2_MethodIsReturn {
 
     public static void main(String[] args) {
         _13_2_MethodIsReturn isReturn1= new _13_2_MethodIsReturn();
+
         String result1 = isReturn1.metotReturnluParametresiz();
         System.out.println(result1);
+
         Integer result2 = isReturn1.metotReturnluParametreli(31313131);
         System.out.println(result2);
     }
