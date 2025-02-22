c442a2fd76d2bbb7ce552e749e78e6ab3ccced75
Egemen Deveci <egemendeveci170@gmail.com>
2025-02-22 13:11:18 +0300

    Strings

--- src/main/java/com/egemendeveci/_2_week/_12_1_String1.java
+++ src/main/java/com/egemendeveci/_2_week/_12_1_String1.java
@@ -8,9 +8,11 @@ public class _12_1_String1 {
         //String vocabulary = new String();
 
         //3. yol
+
+        //STRİNG POOL : BELLEK YÖNETİMİ
         //vocabulary = "Java Öğreniyorum"
-        String vocabulary1 = " Ja va Öğreniyorum ";
-        String vocabulary2 = " Ja va Öğreniyorum ";
+        String vocabulary1 = " Java Öğreniyorum ";
+        String vocabulary2 = " Java Öğreniyorum ";
 
         //lenght()
         System.out.println("Harf sayısı:"+ vocabulary1.length());
@@ -39,5 +41,41 @@ public class _12_1_String1 {
         System.out.println(vocabulary1.trim().substring(2));
         System.out.println(vocabulary1.trim().substring(5));
         System.out.println(vocabulary1.trim().substring(1,10)); // 1<=vocabulayr<=10-1
+
+        String vocabulary3="java";
+        if(vocabulary3.length()>=10) {
+            //StringIndexOutOfBoundsException
+            System.out.println(vocabulary3.substring(1, 10));// 1<=vocabulayr<=10-1
+        }else {
+            System.out.println("verdiğiniz kelime en fazlaa :" + vocabulary3.length() + "karakter vardır");
+        }
+        //contains : içinde Java var mi diye kontrol etttim true çıktı
+        System.out.println(vocabulary1.contains("Java"));
+
+        //Split
+
+        //Replace: İstediğim kelimeyi bir kereliğine bunla değiştirdim. gerçegi degismedi.
+        System.out.println("Değiştir:"+vocabulary1.replace("Java","Java 2025"));
+
+        //birlestirme
+        String concatenation= vocabulary1+vocabulary2+vocabulary3;
+        System.out.println(concatenation);
+        System.out.println(vocabulary1.concat("jsp").concat("jsf"));
+
+        //birlestirme:StringBuilder( en mantıklısı bu)
+        StringBuilder stringBuilder= new StringBuilder();
+        stringBuilder.append(vocabulary1).append("jsp").append("jsf");
+        String dataTostring1= stringBuilder.toString();
+        System.out.println(dataTostring1);
+        //birlestirme: StringBuffer
+        StringBuffer stringBuffer= new StringBuffer();
+        stringBuffer.append(vocabulary1).append("jsp").append("jsf");
+        String dataTostring2= stringBuffer.toString();
+        System.out.println(dataTostring2);
+
+        //hashcode: her nesnennin parmak izi
+        System.out.println(vocabulary1.hashCode());
+        System.out.println(vocabulary2.hashCode());
+        System.out.println(vocabulary3.hashCode());
     }
 }
